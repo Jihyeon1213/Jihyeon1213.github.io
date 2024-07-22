63cbe203990ed1eec778e9467988d81dd13bdd6b
Jihyeon1213 <dns12134@gmail.com>
2024-07-22 20:02:28 +0900

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,12 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+    apiKey: "AIzaSyC_3P3Gec9HK4qqOFsF3kMHmaAT9OhNe0Y",
+    authDomain: "vacotalk.firebaseapp.com",
+    projectId: "vacotalk",
+    storageBucket: "vacotalk.appspot.com",
+    messagingSenderId: "879194316243",
+    appId: "1:879194316243:web:2e23314e2a479cd22ca326",
+    databaseURL: "https://vacotalk-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  };
+  
+
